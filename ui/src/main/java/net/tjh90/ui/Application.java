9a2647bac394cc33e87e078a1b2b15473508b713
tjh90 <tjhughes1990@gmail.com>
2025-05-05 19:44:02 +0100

    Add production build

--- ui/src/main/java/net/tjh90/ui/Application.java
+++ ui/src/main/java/net/tjh90/ui/Application.java
@@ -9,17 +9,29 @@ import org.springframework.context.annotation.Bean;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.AppShellSettings;
 import com.vaadin.flow.theme.Theme;
+
+import net.tjh90.ui.views.home.HomeView;
 
 /// Main application class.
 @SpringBootApplication
 @Theme(value = "default")
-@CssImport("fontawesome/css/all.css")
+@CssImport("./fontawesome/css/all.css")
 @StyleSheet("https://fonts.googleapis.com/css?family=Gloria%20Hallelujah")
 public class Application implements AppShellConfigurator {
 
+    private static final String FAVICON = "favicon.ico";
+    private static final String FAVICON_SIZE = "256x256";
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void configurePage(final AppShellSettings settings) {
+        settings.setPageTitle(HomeView.TITLE);
+        settings.addFavIcon(FAVICON, FAVICON, FAVICON_SIZE);
     }
 
     @Bean
